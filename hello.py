46c65ead9a3bd59005ae69ded411a44990ee3ba0
jouyishih <66415361+jouyishih@users.noreply.github.com>
2020-06-30 13:34:08 +0200

    quicktest

---
+++ hello.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 30 08:56:30 2020
+
+@author: SEJOUSHI
+"""
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Hello google")
+
+# Main program starts here
+hello()
